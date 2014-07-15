626bc0434acc1ba71a67cc955b875484de9c2b32
seanless <seanless@outlook.com>
2014-07-15 22:32:45 +0800

    test

---
+++ cqgtdag/src/com/ystech/core/model/Table1.java
@@ -0,0 +1,31 @@
+package com.ystech.core.model;
+
+public class Table1 implements java.io.Serializable {
+	private String column1;
+	private String column2;
+	private String column3;
+	
+	public String  getColumn1(){
+		return this.column1;
+	}
+	
+	public void setColumn1(String value){
+		this.column1=value;
+	}
+	
+	public String  getColumn2(){
+		return this.column2;
+	}
+	
+	public void setColumn2(String value){
+		this.column2=value;
+	}
+	
+	public String  getColumn3(){
+		return this.column3;
+	}
+	
+	public void setColumn3(String value){
+		this.column3=value;
+	}
+}
